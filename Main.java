fdbeb913acf92326981bf80f2f9d30be47ccb1b7
Ungureanu Petru-Alexandru <ungureanu.petru.alexandru@gmail.com>
2022-06-06 16:25:23 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+                System.out.println("Petru");
+
+                int a = 33;
+                int b = 44;
+                System.out.println(a+b);
+
+                int c = 15;
+                int d = 4;
+                System.out.println(c/d);
+
+                int e =-5;
+                int f = 8;
+                int g = 6;
+                System.out.println(e+f*g);
+
+                int h =55;
+                int i = 9;
+                int j = 9;
+                System.out.println((h+i)%j);
+
+                int k = 20;
+                int l = -3;
+                int m = 5;
+                int n = 8;
+                System.out.println(k+l*m/n);
+
+                int o = 5;
+                int p = 15;
+                int q = 3;
+                int r = 2;
+                int s = 8;
+                int t = 3;
+                System.out.println(o+p/q*r-s%t);
+    }
+}
